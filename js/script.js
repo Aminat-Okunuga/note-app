e55688c3f9cf3d3db96fec9ad80566c8ad4b2934
Aminat Okunuga <makadeaminat@gmail.com>
2024-02-27 20:44:03 +0100

    display note, update the note in localstorage and add line break on clicking enter key

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+// display note from local storage
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+// update the note in local storage
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+showNotes();
+
+// dynamically create the textarea on clicking on create Notes
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -11,8 +23,26 @@ createBtn.addEventListener("click", ()=>{
     notesContainer.appendChild(inputBox).appendChild(img);
 })
 
+// delete textarea on clicking on delete icon
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName ==="p"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nth => {
+            nth.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+// onclicking on enter key add a line break
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
